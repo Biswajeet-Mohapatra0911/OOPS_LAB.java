db0247131be122a584b9d189e7431845b377802c
BISWAJEET MOHAPATRA.R <mohapatrarashmi050@gmail.com>
2025-12-12 13:08:34 +0530

    NAME - BISWAJEET MOHAPATRA.R ROLL NO - 24051402
    LAB - 01 COMPLETED

---
+++ DETAILS.java
@@ -0,0 +1,13 @@
+public class Details {
+    public static void main(String[] args) {
+        String name = "YourName";
+        String rollNo = "YourRollNo";
+        String section = "YourSection";
+        String branch = "YourBranch";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Section: " + section);
+        System.out.println("Branch: " + branch);
+    }
+}
