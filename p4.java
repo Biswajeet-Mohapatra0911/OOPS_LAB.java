2a82746037e19df94c91663fc6f0d19379d09e90
BISWAJEET MOHAPATRA.R <mohapatrarashmi050@gmail.com>
2025-12-12 13:20:38 +0530

    NAME - BISWAJEET MOHAPATRA.R ROLL NO - 24051402
    LAB - 01 COMPLETED

---
+++ p4.java
@@ -0,0 +1,18 @@
+public class p4 {
+    public static void main(String[] args) {
+        int num = 121; // change number here
+        int temp = num;
+        int rev = 0;
+
+        while (temp > 0) {
+            rev = rev * 10 + temp % 10;
+            temp = temp / 10;
+        }
+
+        if (rev == num) {
+            System.out.println(num + " is a Palindrome");
+        } else {
+            System.out.println(num + " is NOT a Palindrome");
+        }
+    }
+}
